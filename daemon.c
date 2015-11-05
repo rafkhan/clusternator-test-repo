12e7ef6b98988bce9edb2ee6ebf171a056e82e2b
Rafael Khan <raf.khan.f@gmail.com>
2015-11-05 12:56:48 -0500

    Broken dockerfile? :(

--- daemon.c
+++ daemon.c
@@ -1,31 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+/*void make_daemon(void) {*/
+  /*pid_t pid;*/
+  /*pid_t sid;*/
+
+  /*pid = fork();*/
+  /*if(pid < 0) {*/
+    /*printf("Error: can not fork");*/
+    /*exit(-1);*/
+  /*} else if(pid > 0) {*/
+    /*printf("Started daemon.");*/
+    /*exit(0);*/
+  /*}*/
+
+  /*umask(0);*/
+
+  /*[> TODO open logs <]*/
+
+  /*sid = setsid();*/
+  /*if (sid < 0) {*/
+    /*exit(-1);*/
+  /*}*/
+
+  /*[> TODO close STDIN/OUT file descriptors here <]*/
+/*}*/
 
 int main(void) {
+  int i;
+  i = 0;
 
-  pid_t pid;
-  pid_t sid;
+  printf("STARTING\n");
 
-  pid = fork();
-  if(pid < 0) {
-    printf("Error: can not fork");
-    exit(-1);
-  } else if(pid > 0) {
-    printf("Started daemon.");
-    exit(0);
+  while(1) {
+    printf("%d\n", i);
+    i++;
+    sleep(1);
   }
-
-  umask(0);
-
-  /* TODO open logs */
-
-  sid = setsid();
-  if (sid < 0) {
-    exit(-1);
-  }
-
-  /* TODO close STDIN/OUT file descriptors here */
   
   return 0;
 }
